4d8ae467abf785997b3fa8d3113a6f278d012e6e
benmbiyu <benmbiyu1@gmail.com>
2022-07-30 19:41:46 +0300

    Add hover effects and change font + navbar smooth scroll

---
+++ JS/scripts.js
@@ -0,0 +1,26 @@
+	// Wait for window load
+// $('html').addClass('loading-in-progress');
+// $(window).load(function() {
+// 	// Animate loader off screen
+// 	$(".se-pre-con").fadeOut(3000);
+// });
+// $(window).on("load", function(){
+//     $('html').removeClass('loading-in-progress');
+// });
+// $(window).load(function(){
+//     $('#overlay').fadeOut();
+// });
+
+// document.onreadystatechange = function(){
+//     if (document.readyState !=="complete"){
+//         document.querySelector(
+//             "body").style.visibility = "hidden";
+//         document.querySelector(
+//             '#spinner').style.visibility = "visible";
+//     }else{
+//         document.querySelector(
+//             '#spinner').style.display = "none";
+//         document.querySelector(
+//             'body').style.visibility = "visible";
+//     }
+// };
