d961b79b103b3d5e6b3665ccad1faa3d936979bb
benmbiyu <benmbiyu1@gmail.com>
2022-08-01 17:16:05 +0300

    add loader to website

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,26 +1,4 @@
-	// Wait for window load
-// $('html').addClass('loading-in-progress');
-// $(window).load(function() {
-// 	// Animate loader off screen
-// 	$(".se-pre-con").fadeOut(3000);
-// });
-// $(window).on("load", function(){
-//     $('html').removeClass('loading-in-progress');
-// });
-// $(window).load(function(){
-//     $('#overlay').fadeOut();
-// });
-
-// document.onreadystatechange = function(){
-//     if (document.readyState !=="complete"){
-//         document.querySelector(
-//             "body").style.visibility = "hidden";
-//         document.querySelector(
-//             '#spinner').style.visibility = "visible";
-//     }else{
-//         document.querySelector(
-//             '#spinner').style.display = "none";
-//         document.querySelector(
-//             'body').style.visibility = "visible";
-//     }
-// };
+var load = document.getElementById("loading");
+function loadfun(){
+	load.style.display = 'none'
+}
