9103c26d88fb3db47461f6932b27563aca46727e
benmbiyu <benmbiyu1@gmail.com>
2022-08-01 18:01:58 +0300

    Get rid of FOUC

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,3 +2,13 @@ var load = document.getElementById("loading");
 function loadfun(){
 	load.style.display = 'none'
 }
+
+let domReady = (cb) => {
+	document.readyState === 'interactive' || document.readyState === 'complete'
+	? cb()
+	: document.addEventListener("DOMContentLoaded", cb);
+};
+
+domReady(() => {
+	document.body.style.visibility = 'visible';
+});
